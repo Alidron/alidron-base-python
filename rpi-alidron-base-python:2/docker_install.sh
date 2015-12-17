419cba7d3e3568985c9a663adec87471e32ad616
Axel Voitier <axel.voitier@gmail.com>
2015-12-17 01:35:27 +0100

    Apparently pip in the RPi image is very old (v1.1) and does not have --no-cache-dir option.

--- rpi-alidron-base-python:2/docker_install.sh
+++ rpi-alidron-base-python:2/docker_install.sh
@@ -28,8 +28,9 @@ make
 apt-get update
 apt-get install -y --no-install-recommends $buildDeps
 
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
 find /usr/local \( -type d -a -name test -o -name tests \) -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) -exec rm -rf '{}' +
 apt-get purge -y --auto-remove $buildDeps
 rm -rf /var/lib/apt/lists/*
+rm -rf /root/.cache/pip
