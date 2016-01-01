080e48de971d8374f775c150a0ea66e885e71470
Axel Voitier <axel.voitier@gmail.com>
2016-01-01 17:31:40 +0100

    Back to upstream pyre as it is now monkey patched in ISAC.

--- alidron-base-python:2-alpine/docker_install.sh
+++ alidron-base-python:2-alpine/docker_install.sh
@@ -23,7 +23,7 @@ echo "http://dl-4.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 
 apk --update add python py-pip py-gevent py-zmq
 pip install --no-cache-dir pebble ipaddress
-pip install --no-cache-dir https://git.tinigrifi.org/alidron/pyre/repository/archive.zip
+pip install --no-cache-dir https://github.com/zeromq/pyre/archive/master.zip
 pip install --no-cache-dir https://github.com/aglyzov/netcall/archive/master.zip
 pip install --no-cache-dir pytest pytest-cov
 
